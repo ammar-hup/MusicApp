7045d6412bd502c50a35e18e617a10381ff6d426
Ammar Mahmoud <amar.link8@gmail.com>
2023-11-17 18:16:04 +0200

    changes

--- musicplatform/albums/models.py
+++ musicplatform/albums/models.py
@@ -5,8 +5,8 @@ from artists.models import Artist
 class Album(models.Model):
     name = models.CharField(default='New Album',max_length=200)
     creation_datetime = models.DateTimeField(auto_now_add=True,blank=False)
-    release_datetime = models.DateTimeField(blank=True, null=True , auto_now_add=True)
-    cost = models.FloatField(blank=False)
+    release_datetime = models.DateTimeField(auto_now_add=True)
+    cost = models.FloatField()
     artist = models.ForeignKey(Artist, on_delete=models.DO_NOTHING)
     is_approved = models.BooleanField(default=False,help_text='Approve the album if its name is not explicit')
     def __str__(self):
