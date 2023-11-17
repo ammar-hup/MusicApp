7e755e5e27f8b4afbac46dcde1491f6d9639f4f3
Ammar Mahmoud <amar.link8@gmail.com>
2023-11-17 18:38:24 +0200

    good

--- musicplatform/artists/admin.py
+++ musicplatform/artists/admin.py
@@ -1,16 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
 from .models import Artist
-from albums.models import Album
-
-class AlbumInline(admin.TabularInline):
-    model = Album
 class ArtistAdmin(admin.ModelAdmin):
-    list_display = ['name' , 'socialLink','approved_albums']
-    inlines = [AlbumInline]
+    model = Artist
 
-    def approved_albums(self, obj):
-        return Album.objects.filter(artist=obj, is_approved=True).count()
-
-admin.site.register(Artist,ArtistAdmin)
+admin.site.register(ArtistAdmin)
