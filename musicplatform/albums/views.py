fdd8b27901df85fa1bf1170f9fee48f006ae86ea
Ammar Mahmoud <amar.link8@gmail.com>
2024-01-10 22:50:48 +0200

    updated the albums to class based view

--- musicplatform/albums/views.py
+++ musicplatform/albums/views.py
@@ -2,30 +2,36 @@ from django.http import HttpResponse
 from django.shortcuts import render,redirect
 from .forms import AlbumForm
 from .models import Album
+from django.views.generic import ListView,CreateView
 
 # Create your views here.
-def index(request):
-    return HttpResponse("You're at the Album index.")
-# the form method
-def create_album(request):
-    if request.method == 'POST':
-        form = AlbumForm(request.POST)
-        if form.is_valid():
-            album_name = form.cleaned_data['album_name']
-            cost = form.cleaned_data['cost']
-            artist = form.cleaned_data['artist']
-            is_approved = form.cleaned_data['is_approved']
+class AlbumList(ListView):
+    model = Album
+    context_object_name  = 'albums'
+    template_name = 'albums/view_albums.html'
 
-            # create an Album instance with the retrieved data
-            new_album = Album.objects.create(
-                album_name=album_name,
-                cost=cost,
-                artist=artist,
-                is_approved=is_approved,
-            )
-            return redirect(request.path)  # Redirect to the same page after creating an album , and be able to create another album
-    else:
-        form = AlbumForm()
+    def get_queryset(self):
+        return Album.objects.all()
+class CreateAlbumView(CreateView):
+    model = Album
+    form_class = AlbumForm
+    template_name = 'albums/create_album.html'
+    success_url = '/albums/create/'
 
-    return render(request, 'albums/create_album.html', {'form': form})
-    
+    def form_valid(self, form):
+        album_name = form.cleaned_data['album_name']
+        cost = form.cleaned_data['cost']
+        artist = form.cleaned_data['artist']
+        is_approved = form.cleaned_data['is_approved']
+        # create an Album instance with the retrieved data
+        new_album = Album.objects.create(
+            album_name=album_name,
+            cost=cost,
+            artist=artist,
+            is_approved=is_approved,
+        )
+        return super().form_valid(form)
+
+    def get(self, request, *args, **kwargs):
+        form = self.get_form()
+        return self.render_to_response({'form': form})
