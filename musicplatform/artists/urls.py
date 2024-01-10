b2a6037a4979618674766273b85edb6569e00292
Ammar Mahmoud <amar.link8@gmail.com>
2024-01-10 22:26:13 +0200

    updated the artists to class based view

--- musicplatform/artists/urls.py
+++ musicplatform/artists/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.artist_list, name='artist_list'),
-    path('create/', views.create_artist, name='create_artist'),
+    path('', views.ArtistList.as_view(), name='artistList'),
+    path('create/', views.CreateArtistView.as_view(), name='CreateArtistView'),
 ]
