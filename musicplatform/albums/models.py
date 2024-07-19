4e636a46f2204ffe622979c95a8dd8738298d759
Ammar Mahmoud <amar.link8@gmail.com>
2024-07-19 19:11:03 +0300

    task1 updated

--- musicplatform/albums/models.py
+++ musicplatform/albums/models.py
@@ -4,11 +4,11 @@ from artists.models import Artist
 # Create your models here.
 
 class Album(models.Model):
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
-    album_name = models.CharField(default = 'New Album')
+    album_name = models.CharField(default = 'New Album',max_length=255)
     creation_datetime = models.DateTimeField(auto_now_add = True)
     release_datetime = models.DateTimeField(blank = False)
     cost = models.FloatField(blank = False)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     
     def __str__(self):  
         return self.album_name    
--- musicplatform/artists/models.py
+++ musicplatform/artists/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Artist(models.Model):
-    artist_name = models.CharField(blank = False, unique = True, default='Unknown')
+    artist_name = models.CharField(blank = False, unique = True, max_length = 255, default = 'Unknown')
     socialLink = models.URLField(default="https://www.instagram.com")
 
     def __str__(self):
